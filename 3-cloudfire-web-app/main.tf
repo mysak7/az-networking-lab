8034de1498dae340452c6097c679b4365a1d46ca
mysak7 <michal.burdik@proton.me>
2026-05-12 22:46:16 +0200

    refactor: replace hardcoded Cloudflare IPs with cloudflare_ip_ranges data source

--- 3-cloudfire-web-app/main.tf
+++ 3-cloudfire-web-app/main.tf
@@ -4,36 +4,12 @@ resource "random_string" "suffix" {
   upper   = false
 }
 
-locals {
-  app_name = "${var.prefix}-3-cloudfire-${random_string.suffix.result}"
-  fqdn     = "${var.subdomain}.${var.root_domain}"
+data "cloudflare_ip_ranges" "cf" {}
 
-  # Cloudflare published IP ranges — https://www.cloudflare.com/ips/
-  # Keep in sync with cloudflare.com/ips if ranges change
-  cloudflare_ips = [
-    "173.245.48.0/20",
-    "103.21.244.0/22",
-    "103.22.200.0/22",
-    "103.31.4.0/22",
-    "141.101.64.0/18",
-    "108.162.192.0/18",
-    "190.93.240.0/20",
-    "188.114.96.0/20",
-    "197.234.240.0/22",
-    "198.41.128.0/17",
-    "162.158.0.0/15",
-    "104.16.0.0/13",
-    "104.24.0.0/14",
-    "172.64.0.0/13",
-    "131.0.72.0/22",
-    "2400:cb00::/32",
-    "2606:4700::/32",
-    "2803:f800::/32",
-    "2405:b500::/32",
-    "2405:8100::/32",
-    "2a06:98c0::/29",
-    "2c0f:f248::/32",
-  ]
+locals {
+  app_name       = "${var.prefix}-3-cloudfire-${random_string.suffix.result}"
+  fqdn           = "${var.subdomain}.${var.root_domain}"
+  cloudflare_ips = concat(data.cloudflare_ip_ranges.cf.ipv4_cidr_blocks, data.cloudflare_ip_ranges.cf.ipv6_cidr_blocks)
 }
 
 resource "azurerm_resource_group" "rg" {
